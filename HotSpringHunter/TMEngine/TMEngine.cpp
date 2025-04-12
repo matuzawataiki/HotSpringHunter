d3d83228d83a9f06d8be11fcee3164d6ddac81ae
MatuzawaTaiki <CA01244026@st.kawahara.ac.jp>
2025-04-12 17:58:59 +0900

    プリコンパイラの適応
    新しくプロジェクトを作って、設定を変更

---
+++ HotSpringHunter/TMEngine/TMEngine.cpp
@@ -0,0 +1,7 @@
+﻿#include "TMEnginePreCompile.h"
+#include "TMEngine.h"
+
+namespace nsTMEngine
+{
+	SceneLight* g_sceneLight = nullptr;
+}
---
+++ HotSpringHunter/TMEngine/TMEngine.h
@@ -0,0 +1,9 @@
+#pragma once
+namespace nsTMEngine {
+	class SceneLight;
+	class TMEngine
+	{
+	};
+
+	extern SceneLight* g_sceneLight;
+}
