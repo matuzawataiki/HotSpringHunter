596aebd12b67199f8bec884bb66865e646821d0b
ShimookaMinami <CA01244013@st.kawahara.ac.jp>
2025-07-08 15:41:54 +0900

    savedata実装

--- HotSpringHunter/Game/common/SaveData.h
+++ HotSpringHunter/Game/common/SaveData.h
@@ -3,21 +3,42 @@
 class SaveData
 {
 private:
-	SaveData() {}
-	~SaveData() {}
+	SaveData();
+	~SaveData();
+
+	void Serialize();
+	void Deserialize();
+
 
 public:
-	float GetResultTime()const  //リザルトタイムを取得(constを右に置くと関数内の数字は変わらない)
+	//リザルトタイムを取得(constを右に置くと関数内の数字は変わらない)
+	float GetResultTime()const
 	{
 		return m_resultTime;
 	}
-	void SetResultTime(const float time) //リザルトタイムを設定
+	//リザルトタイムを設定
+	void SetResultTime(const float time)
 	{
 		m_resultTime = time;
+		//結果時間リストに追加
+		m_resultTimeList.push_back(time);
+	}
+
+	// リザルトタイムのリストを取得
+	// ランキングなどを表示する際に使用する
+	// 授業でならったvector使ってみた！便利！
+	const std::vector<float>& GetResultTimeList() const
+	{
+		return m_resultTimeList;
 	}
 private:
-	float m_resultTime; //結果時間
+	float m_resultTime;						//結果時間
+	std::vector<float> m_resultTimeList;	//結果時間リスト。保存している情報
 
+
+	/**
+	 * シングルトン用
+	 */
 public:
 /// <summary>
 /// インスタンスを作る
