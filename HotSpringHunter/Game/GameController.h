4f30298e03b1020b51846f95737677e7ff8507af
ShimookaMinami <CA01244013@st.kawahara.ac.jp>
2025-09-06 17:35:06 +0900

    コントローラー修正等

---
+++ HotSpringHunter/Game/GameController.h
@@ -0,0 +1,20 @@
+// TODO: 削除予定
+//#pragma once
+//class Game;
+//class SoundEffect;
+//
+//class GameController:public IGameObject
+//{
+//public:
+//	GameController();
+//	~GameController();
+//	bool Start() override;
+//	void Update() override;
+//	void Render(RenderContext& rc)override;
+//
+//private:
+//	Game*        m_game        = nullptr;
+//	SoundEffect* m_soundEffect = nullptr;
+//	SpriteRender m_controllerModel;
+//};
+//
